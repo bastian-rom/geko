e1d2a43733d4335121bf5864c1906f8fba535b6c
Sebastián Romero <romsebastian@gmail.com>
2025-11-24 18:42:01 -0500

    Create extensions.py

---
+++ extensions.py
@@ -0,0 +1,11 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+from flask_admin import Admin
+
+db = SQLAlchemy()
+migrate = Migrate()
+login_manager = LoginManager()
+bcrypt = Bcrypt()
+admin = Admin(name='Geko Admin', template_mode='bootstrap3')
